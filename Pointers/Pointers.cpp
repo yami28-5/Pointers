164af567ee76e9fa38df4ce56d00c167120fffb0
Богдан пензин <bogdanpenzin432@gmail.com>
2025-09-08 14:54:05 +0700

    Add project files.

---
+++ Pointers/Pointers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+//#define POINTERS_BASICS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+
+#ifdef POINTERS_BASICS
+	cout << "Hello Pointers" << endl;
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;
+	cout << pa << endl;
+	cout << *pa << endl;
+
+	int* pd;      //обьявление указателя
+	int b = 3;
+	*pd = &b;
+	cout << pd << endl;
+	cout << *pd << endl;
+#endif // POINTERS_BASICS
+	const int n = 5;
+	short arr[n] = { 3 , 5, 8 ,13 ,21 };
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << tab;
+	}
+	cout << endl;
+}
