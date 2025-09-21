9e614c51c71530f402a50894ef3a3c40e3475ca3
Богдан пензин <bogdanpenzin432@gmail.com>
2025-09-22 00:39:42 +0700

    DynamicMemory_1.1

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 #define tab "\t"
 
 void FiilRand(int arr[], const int n);
+void FillRand(int** arr, const int ROWS, const int COLS);
 
 void Print(int arr[], const int n);
+void Print(int** arr, const int ROWS, const int COLS);
 
 int* push_back(int arr[], int& n, int value);
 int* push_front(int arr[], int& n, int value);
@@ -15,21 +17,23 @@ int* pop_back(int arr[], int& n);
 int* pop_front(int arr[], int& n);
 int* erase(int arr[], int& n, int index);
 
+#define DYNAMIC_MEMORY_1
+//#define DYNAMIC_MEMORY_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DYNAMIC_MEMORY_1
 	cout << "DynamicMemory" << endl;
 	int n = 5;
 	cout << "¬ведите размер массива: "; cin >> n;
 	int* arr = new int[n];
+
 	FiilRand(arr, n);
 	Print(arr, n);
 
 	int value;
 	cout << "¬ведите добавл€м€емое значений: "; cin >> value;
-
-
-
 	arr = push_back(arr, n, value);
 	Print(arr, n);
 
@@ -44,9 +48,35 @@ void main()
 	Print(arr, n);
 
 	Print(arr = pop_back(arr, n), n);
+
 	Print(arr = pop_front(arr, n), n);
+
 	cout << "¬ведите индекс удал€емого элемента: "; cin >> index;
 	Print(arr = erase(arr, n, index), n);
+	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+	int rows;
+	int cols;
+	cout << "¬ведите количество строк: "; cin >> rows;
+	cout << "¬ведите количество элементов строки: "; cin >> cols;
+
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+#endif // DYNAMIC_MEMORY_2
 }
 
 
@@ -59,11 +89,34 @@ void FiilRand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void FillRand(int** arr, const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		cout << *(arr + i) << tab;
+	}
+	cout << endl;
+}
+void Print(int** arr, const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
 	}
 	cout << endl;
 }
